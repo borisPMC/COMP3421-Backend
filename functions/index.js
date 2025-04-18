e73d62bd5a9a0696fafaefeebf5713fdedfb6e59
borisPMC <borisleung30@gmail.com>
2025-04-18 20:23:19 +0800

    modified:   firebase.json 	new file:   functions/.eslintrc.js 	new file:   functions/.gitignore 	new file:   functions/index.js 	new file:   functions/package-lock.json 	new file:   functions/package.json 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+initializeApp();
+
+const aTuringRef = db.collection('qa').doc('0');
+
+await aTuringRef.set({
+  'alt_A': 'Berlin',
+  'alt_B': 'Madrid',
+  'alt_C': 'London',
+  'question': "What is the capital of France?",
+  'answer': 'Paris',
+})
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
